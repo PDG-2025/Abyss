308140bd2dfd50f7b1ccb656bd85a0decee64580
Arno Tribolet <arno.tribolet@hes-so.ch>
2025-08-23 11:28:54 +0200

    Basic python app

---
+++ firmware/src/hello_world/__init__.py
@@ -0,0 +1,2 @@
+"""Workflow Demo App"""
+__version__ = "0.1.0"
---
+++ firmware/src/hello_world/main.py
@@ -0,0 +1,14 @@
+def hello_world():
+	return "Hello World !"
+	
+def bye():
+	return "Bye"	
+	
+def main():
+	print(hello_world())
+	time.sleep(10)
+	print(bye())
+	time.sleep(5)
+	
+if __name__ = "__main__":
+	main()	
